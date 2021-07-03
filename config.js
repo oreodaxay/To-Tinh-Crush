9b766e8aeb33e5156b76006454c14714c482565e
oreodaxay <86867412+oreodaxay@users.noreply.github.com>
2021-07-03 15:14:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Heyyy baeee',
+    introDesc: ` Em đang ăn chay hay chăn ai?
+    Nói lời ngọt ngào hay punchline?
+    Ngàn lẻ một đêm hay one night?
+    Không ăn được em thì ăn ai?`,
+    btnIntro: 'luv uuuu',
+    title: 'Can I call you baby?
+            Can you be my friend?
+            Can you be my lover up until the very end?
+            Let me show you love, oh, I don't pretend',
+    desc: 'Don't you worry
+           I'll be there, whenever you want me ',
+    btnYes: 'Vẫn cứ là yêu em <33',
+    btnNo: 'Không,chị em à :3',
+    question:'Oh baby girl tonight u are my princess
+              Ghi tên em vào guinness ... của chị ... của chị <3',
+    btnReply: 'send for my lover <3',
+    reply: 'Em ơi ta hãy yêu như chỉ có đêm nay được yêu <33333333',
+    mess: 'Chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h chị qua đón nhé công chúa.',
+    btnAccept: 'Oceeee lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100050298278633/
 }
